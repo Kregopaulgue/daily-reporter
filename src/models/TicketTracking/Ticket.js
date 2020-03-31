734af3e8e602ab73194752b975d91e2759f1b484
Paul <kregopaulgue@gmail.com>
2020-03-31 19:01:08 +0300

    implement ticket model

---
+++ src/models/TicketTracking/Ticket.js
@@ -0,0 +1,42 @@
+/**
+ * Represents ticket from Jira
+ * @class
+ */
+class Ticket {
+    /**
+     * @constructor
+     * @param {number} id id(number) of the ticket
+     * @param {string} name Name of the ticket 
+     * @param {string} url Url of the ticket
+     * @param {array} [logs] Array of logs (<code>Log</code> class object), that are tracked in a ticket
+     */
+    constructor(id, name, url, logs) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+
+        // If logs are passes - set them. Empty array otherwise
+        this.logs = logs ? logs : [];
+    }
+
+    /**
+     * Adds log to the ticket
+     * @param {Log} log Object of log class 
+     */
+    addLog(log) {
+        this.logs.push(log);
+    }
+
+    /**
+     * Deletes passed log from ticket
+     * @param {Log} logToDelete 
+     */
+    deleteLog(logToDelete) {
+        const indexOfLogToDelete = this.logs.findIndex(log => {
+            log.id = logToDelete.id;
+        });
+
+        //deleting selected log from array
+        this.logs.splice(indexOfLogToDelete, 1);
+    }
+}
