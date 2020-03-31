95f1a0d1684103b17b3b9db611625c93aa83e4a3
Paul <kregopaulgue@gmail.com>
2020-03-31 19:10:30 +0300

    implement PlanToDo model

---
+++ src/models/PlanToDo.js
@@ -0,0 +1,14 @@
+/**
+ * @class
+ */
+class PlanToDo {
+    /**
+     * @constructor
+     * @param {number} id Id of the plan inside a ticket
+     * @param {string} planToDo
+     */
+    constructor(id, planToDo) {
+        this.id = id;
+        this.planToDo = planToDo;
+    }
+}
