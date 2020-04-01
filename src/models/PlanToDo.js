f884811d56dd551624785cade79ef5e119669956
Paul <kregopaulgue@gmail.com>
2020-04-01 10:01:35 +0300

    fix planToDo argument passing

--- src/models/PlanToDo.js
+++ src/models/PlanToDo.js
@@ -5,10 +5,12 @@ class PlanToDo {
     /**
      * @constructor
      * @param {number} id Id of the plan inside a ticket
-     * @param {string} planToDo
+     * @param {string} [planToDo]
      */
     constructor(id, planToDo) {
         this.id = id;
-        this.planToDo = planToDo;
+
+        //if no plan to do is passed - set empty string
+        this.planToDo = planToDo ? planToDo : '';
     }
 }
