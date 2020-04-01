aa37933da7c258d5d0cba915c2a7ecbca344f05d
Paul <kregopaulgue@gmail.com>
2020-04-01 10:05:26 +0300

    fixed optional arguments on Log constrctor

--- src/models/TicketTracking/Log.js
+++ src/models/TicketTracking/Log.js
@@ -6,12 +6,15 @@ class Log {
     /**
      * @constructor
      * @param {number} id Id of the log in a ticket 
-     * @param {string} description Content of a jira log 
-     * @param {string} time Time being loged in format 1h 1m 1s
+     * @param {string} [description] Content of a jira log 
+     * @param {string} [time] Time being loged in format 1h 1m 1s
      */
     constructor(id, description, time) {
         this.id = id;
-        this.description = description;
-        this.time = time;
+        
+        //if these arguments are passed - set
+        //empty strings otherwise
+        this.description = description ? description : '';
+        this.time = time ? time : '';
     }
 }
